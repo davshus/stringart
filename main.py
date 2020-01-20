85435426c65a0c1aed9aeca0db58944910e9b190
David Shustin <david.shustin@gmail.com>
2020-01-20 01:52:36 -0500

    Experimenting

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from artboard import Artboard, Yarn
 n = 180
 board = Artboard(304.8, n)
 for i in range(0, n):
-    board.add_string(0, i)
+    board.add_string(i, (i + 45) % n)
 # board.add_string(0, 45)
 print(board.render(background='#FFFFFF').savePng('build/test.png'))
